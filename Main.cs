8530c0a1674540333be1636cfc148dd3444038d6
lenoob <78706766+lenoobwastaken@users.noreply.github.com>
2022-07-27 15:36:25 -0400

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,51 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using ABI_RC.Core.Player;
+using DarkRift;
+using System.IO;
+using System.Net;
+using RootMotion.FinalIK;
+using ABI_RC.Core.Networking;
+using System;
+using System.Linq;
+
+namespace CVRKeyBinds
+{
+    public static class BuildInfo
+    {
+        public const string Name = "CVRESP"; 
+        public const string Description = "Mod for Testing"; 
+        public const string Author = "Lenoob and CVRMG"; 
+        public const string Company = "CVRMG"; 
+        public const string Version = "1.0.0"; 
+        public const string DownloadLink = null;
+    }
+
+    public class CVRKeyBinds : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("\n CTRL + F to fly \n CTRL + LEFTCLICK to TP");
+        }
+        public override void OnUpdate()
+        {
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                Ray r = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+                RaycastHit rhit;
+                bool IsHit = Physics.Raycast(r, out rhit);
+                if (IsHit)
+                {
+                   ABI_RC.Systems.MovementSystem.MovementSystem.Instance.TeleportTo(rhit.point);
+                }
+            }
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F))
+            {
+                ABI_RC.Systems.MovementSystem.MovementSystem.Instance.ToggleFlight();
+            }
+        }
+     
+    }
+}
